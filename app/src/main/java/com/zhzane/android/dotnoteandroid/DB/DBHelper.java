c157d74806ab73d2d669a8e63984ab7271b9d62f
张宏展 <zhzane@outlook.com>
2016-02-28 21:21:22 +0800

    Revert "第一次总结"
    This reverts commit e019b7206358946ff9b20e3d401ffdbd69ac53e1.

--- app/src/main/java/com/zhzane/android/dotnoteandroid/DB/DBHelper.java
+++ app/src/main/java/com/zhzane/android/dotnoteandroid/DB/DBHelper.java
@@ -19,18 +19,12 @@ public class DBHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE IF NOT EXISTS Bill" +
 <<<<<<< HEAD
-<<<<<<< HEAD
                 "(id INTEGER PRIMARY KEY AUTOINCREMENT,BillNo VARCHAR,UserId VARCHAR,Money DECIMAL,CreateTime VARCHAR," +
                 "LastModifiedTime VARCHAR,ExternalId VARCHAR,TagId VARCHAR)");
         //创建用户表（User）
         db.execSQL("CREATE TABLE IF NOT EXISTS User" +
                 "(id INTEGER PRIMARY KEY AUTOINCREMENT,UserId VARCHAR,UserName VARCHAR,TotalMoney DECIMAL,RelatedUserId VARCHAR,MAC VARCHAR)");
         //创建标签表（Tag）
-=======
-                "(id INTEGER PRIMARY KEY AUTOINCREMENT,BillNo VARCHAR,UserId VARCHAR,Money DECIMAL,CreateTime VARCHAR,LableId VARCHAR)");
-        db.execSQL("CREATE TABLE IF NOT EXISTS User" +
-                "(id INTEGER PRIMARY KEY AUTOINCREMENT,UserId VARCHAR,UserName VARCHAR,TotalMoney DECIMAL,RelatedUserId VARCHAR,MAC VARCHAR)");
->>>>>>> parent of cd93d0b... Merge pull request #3 from HarryKwens/kwens
 =======
                 "(id INTEGER PRIMARY KEY AUTOINCREMENT,BillNo VARCHAR,UserId VARCHAR,Money DECIMAL,CreateTime VARCHAR,LableId VARCHAR)");
         db.execSQL("CREATE TABLE IF NOT EXISTS User" +
